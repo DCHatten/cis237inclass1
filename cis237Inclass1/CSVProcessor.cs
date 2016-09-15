7d0d16dc8e2d84ad2f1bdeed477e6bc773a72142
David Hatten <dhatten87@gmail.com>
2016-09-15 17:07:28 -0400

    Added a .csv processor to read and parse a file

---
+++ cis237Inclass1/CSVProcessor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class CSVProcessor
+    {
+        //Setting the bath for the reader
+        private string path = Path.Combine(Environment.CurrentDirectory + "../../../../datafiles/employees.csv");
+
+        public bool ImportCSV(Employee[] employees)
+        {
+            //Declare a variable for the stream reader.
+            StreamReader streamReader = null;
+            //Start a try to verify the path to the file.
+            //Throw an exception if incorrect.
+            try
+            {
+                //Declare a string for each line we will read in.
+                string line;
+                //Instantiate the stream reader with the file path.
+                streamReader = new StreamReader(this.path);
+                //Setup a counter.
+                int counter = 0;
+                //While loop for processing the data in the file
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    this.processLine(line, employees, counter);
+                    counter++;
+                }
+                //All the reads are successful, return true.
+                return true;
+            }
+            catch (Exception e)
+            {
+                //Output the exception and the stack trace.
+                Console.WriteLine(e.ToString());
+                Console.WriteLine();
+                Console.WriteLine(e.StackTrace);
+                //Return false, reading failed
+                return false;
+            }
+            //Ensuring the stream reader gets closed regardless of try outcome.
+            finally
+            {
+                //Check to make sure the stream was instantiated.
+                if (streamReader != null)
+                {
+                    //Close the reader
+                    streamReader.Close();
+                }
+            }
+        }
+        //Method for processing each line of text from the CSV file.
+        private void processLine (string line, Employee[] employees, int index)
+        {
+            //Declare a string array and assigne the spline line to it.
+            string[] parts = line.Split(',');
+            //Assign the parts to local variables that mean something
+            string firstName = parts[0];
+            string lastName = parts[1];
+            decimal salary = decimal.Parse(parts[2]);
+            //Use the variables to create a new employee in the employees array at the index point that was passed in.
+            employees[index] = new cis237Inclass1.Employee(firstName, lastName, salary);
+
+        }
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -16,18 +16,21 @@ namespace cis237Inclass1
             //Console.WriteLine(myEmployee.Reporting());
             //Console.WriteLine(myEmployee);
 
-            Employee[] employees = new Employee[10];
+            Employee[] employees = new Employee[5];
+            CSVProcessor processor = new CSVProcessor();
+            processor.ImportCSV(employees);
 
-            employees[0] = new Employee("James", "Kirk", 453.00m);
-            employees[1] = new Employee("Jean-Luc", "Picard", 607.00m);
-            employees[2] = new Employee("Benjamin", "Sisko", 556.00m);
-            employees[3] = new Employee("Kathryn", "Janeway", 290.00m);
-            employees[4] = new Employee("Johnathan", "Archer", 180.00m);
-            employees[5] = new Employee("Geordi", "LeForge", 190.00m);
-            employees[6] = new Employee("Wesly", "Crusher", 1.00m);
-            employees[7] = new Employee("Benjamin", "Sulu", 200.00m);
-            employees[8] = new Employee("Mister", "Spock", 250.00m);
-            employees[9] = new Employee("Beverly", "Crusher", 225.00m);
+
+            //employees[0] = new Employee("James", "Kirk", 453.00m);
+            //employees[1] = new Employee("Jean-Luc", "Picard", 607.00m);
+            //employees[2] = new Employee("Benjamin", "Sisko", 556.00m);
+            //employees[3] = new Employee("Kathryn", "Janeway", 290.00m);
+            //employees[4] = new Employee("Johnathan", "Archer", 180.00m);
+            //employees[5] = new Employee("Geordi", "LeForge", 190.00m);
+            //employees[6] = new Employee("Wesly", "Crusher", 1.00m);
+            //employees[7] = new Employee("Benjamin", "Sulu", 200.00m);
+            //employees[8] = new Employee("Mister", "Spock", 250.00m);
+            //employees[9] = new Employee("Beverly", "Crusher", 225.00m);
 
             //foreach (Employee employee in employees)
             //{
